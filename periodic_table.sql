7af5e2a49fa798fe2b577c105d82f59f318ec781
natev1p8r <natnaelworkua@gmail.com>
2025-03-14 08:45:04 +0300

    Update periodic_table.sql

--- periodic_table.sql
+++ periodic_table.sql
@@ -1,8 +1,9 @@
-psql --username=freecodecamp --dbname=periodic_table
+
 ALTER TABLE properties RENAME weight TO atomic_mass;
 ALTER TABLE properties RENAME melting_point TO melting_point_celsius;
 ALTER TABLE properties RENAME boiling_point TO boiling_point_celsius;
-ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET TO NOT NULL;
+ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
+ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
 ALTER TABLE elements ADD CONSTRAINT unique_name UNIQUE(name);
 ALTER TABLE elements ADD CONSTRAINT unique_symbol  UNIQUE(symbol);
 ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;
@@ -12,14 +13,15 @@ CREATE TABLE types (type_id  INT PRIMARY KEY, type VARCHAR(50) NOT NULL);
 
 INSERT INTO types( type_id, type) VALUES(1, 'metal'), (2, 'nonmetal'),(3, 'metalloid');
 ALTER TABLE properties ALTER COLUMN atomic_mass TYPE DECIMAL;
-
+ALTER TABLE properties ADD FOREIGN KEY (type_id) REFERENCES types(type_id);
 UPDATE elements SET symbol = INITCAP(symbol);
 UPDATE elements SET name = UPPER(LEFT(name, 1)) || LOWER(SUBSTRING(name FROM 2));
-UPDATE properties SET atomic_mass = ROUND(atomic_mass, 2);
 INSERT INTO elements VALUES (9,'F', 'Fluorine');
 INSERT INTO properties VALUES (9, 'nonmetal', 18.998, -220, -188.1);
 INSERT INTO elements VALUES(10, 'Ne', 'Neon');
 INSERT INTO properties VALUES(10, 'nonmetal', 20.18, -248.6, -246.1);
+ALTER TABLE properties ADD COLUMN type_id INT;
+ALTER TABLE properties ADD FOREIGN KEY (type_id) REFERENCES types(type_id);
 
 UPDATE properties SET type_id = 1 WHERE type = 'metal';
 UPDATE properties SET type_id = 2 WHERE type = 'nonmetal';
@@ -28,6 +30,9 @@ UPDATE properties SET type_id = 3 WHERE type = 'metalloid';
 DELETE FROM properties WHERE atomic_number = 1000;
 DELETE FROM elements WHERE atomic_number = 1000;
 ALTER TABLE properties DROP COLUMN type;
+
+
+
 
 
 
